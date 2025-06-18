8a0b8ad82dc63a3a660c7dd483bda35028fd54aa
lumo <lumosan@proton.me>
2025-06-18 15:32:38 +0200

    add more lens functions

--- src/periphery/EulaunchLens.sol
+++ src/periphery/EulaunchLens.sol
@@ -1,12 +1,33 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.27;
 
+import {ERC20} from "solady/tokens/ERC20.sol";
 import {IEulerSwapPeriphery} from "euler-swap/src/interfaces/IEulerSwapPeriphery.sol";
 import {IEulerSwap} from "euler-swap/src/interfaces/IEulerSwap.sol";
 import {Eulaunch} from "../Eulaunch.sol";
 import {LiquidityManager, Resources} from "../LiquidityManager.sol";
 import {TokenSuiteFactory} from "../TokenSuiteFactory.sol";
 import {ICreateX} from "../vendor/ICreateX.sol";
+
+struct PoolState {
+    // Resources
+    address eulerSwap;
+    address liquidityManager;
+    address baseToken;
+    address baseVault;
+    address quoteToken;
+    address quoteVault;
+    // Curve parameters
+    uint112 equilibriumReserveBase;
+    uint112 equilibriumReserveQuote;
+    uint256 priceBase;
+    uint256 priceQuote;
+    uint256 concentrationBase;
+    uint256 concentrationQuote;
+    // Reserves
+    uint256 reserveBase;
+    uint256 reserveQuote;
+}
 
 /// @title EulaunchLens
 /// @notice A data entry aggregator for Eulaunch.
@@ -55,13 +76,69 @@ contract EulaunchLens {
         }
     }
 
+    /// @notice Gets the state of the pool.
+    /// @param pool The address of the pool.
+    /// @return state The state of the pool.
+    function getPoolState(address pool) public view returns (PoolState memory state) {
+        Resources memory resources = Eulaunch(eulaunch).getResourcesByPool(pool);
+        state.eulerSwap = resources.eulerSwap;
+        state.liquidityManager = resources.liquidityManager;
+        state.baseToken = resources.baseToken;
+        state.baseVault = resources.baseVault;
+        state.quoteToken = resources.quoteToken;
+        state.quoteVault = resources.quoteVault;
+
+        (state.reserveBase, state.reserveQuote) = getReserves(state.baseToken, state.quoteToken);
+        IEulerSwap.Params memory params = getParams(state.baseToken, state.quoteToken);
+
+        if (switcheroo(state.baseToken, state.quoteToken)) {
+            state.equilibriumReserveBase = params.equilibriumReserve1;
+            state.equilibriumReserveQuote = params.equilibriumReserve0;
+            state.priceBase = params.priceY;
+            state.priceQuote = params.priceX;
+            state.concentrationBase = params.concentrationY;
+            state.concentrationQuote = params.concentrationX;
+        } else {
+            state.equilibriumReserveBase = params.equilibriumReserve0;
+            state.equilibriumReserveQuote = params.equilibriumReserve1;
+            state.priceBase = params.priceX;
+            state.priceQuote = params.priceY;
+            state.concentrationBase = params.concentrationX;
+            state.concentrationQuote = params.concentrationY;
+        }
+    }
+
+    /// @notice Gets the state of the pool by base token.
+    /// @param token The address of the base token.
+    /// @return state The state of the pool.
+    function getPoolStateByBaseToken(address token) external view returns (PoolState memory state) {
+        address pool = Eulaunch(eulaunch).getPoolByBaseToken(token);
+        require(pool != address(0), PoolNotFound());
+        state = getPoolState(pool);
+    }
+
+    /// @notice Calculates the avg price of buying `tokenOut` with `amountIn` of `tokenIn`.
+    /// @param tokenIn The address of the input token.
+    /// @param tokenOut The address of the output token.
+    /// @param amountIn The amount of input tokens.
+    /// @return price The price of buying `tokenOut` with `amountIn` of `tokenIn` in WAD, decimals of tokens are normalized to 18.
+    function getPrice(address tokenIn, address tokenOut, uint256 amountIn) external view returns (uint256 price) {
+        uint256 amountOut = quoteExactInput(tokenIn, tokenOut, amountIn);
+        // Either side of the tokenIn/Out can be 18 decimals or 6 decimals, so we need to normalize the decimals
+        uint256 decimalsIn = ERC20(tokenIn).decimals();
+        uint256 decimalsOut = ERC20(tokenOut).decimals();
+        uint256 normalizedAmountIn = amountIn * 10 ** (18 - decimalsIn);
+        uint256 normalizedAmountOut = amountOut * 10 ** (18 - decimalsOut);
+        price = normalizedAmountOut * 10 ** 18 / normalizedAmountIn;
+    }
+
     /// @notice Gets the amount of output tokens for a given amount of input tokens.
     /// @param tokenIn The address of the input token.
     /// @param tokenOut The address of the output token.
     /// @param amountIn The amount of input tokens.
     /// @return amountOut The amount of output tokens.
     function quoteExactInput(address tokenIn, address tokenOut, uint256 amountIn)
-        external
+        public
         view
         returns (uint256 amountOut)
     {
@@ -113,13 +190,9 @@ contract EulaunchLens {
     /// @notice Gets the reserves of the pool.
     /// @param tokenA The address of the first token.
     /// @param tokenB The address of the second token.
-    /// @return reserveIn The reserve of the first token.
-    /// @return reserveOut The reserve of the second token.
-    function getReserves(address tokenA, address tokenB)
-        external
-        view
-        returns (uint256 reserveIn, uint256 reserveOut)
-    {
+    /// @return reserveA The reserve of the first token.
+    /// @return reserveB The reserve of the second token.
+    function getReserves(address tokenA, address tokenB) public view returns (uint256 reserveA, uint256 reserveB) {
         address eulerSwap = getPool(tokenA, tokenB);
         require(eulerSwap != address(0), PoolNotFound());
         (uint112 reserve0, uint112 reserve1,) = IEulerSwap(eulerSwap).getReserves();
@@ -128,6 +201,16 @@ contract EulaunchLens {
         } else {
             return (reserve0, reserve1);
         }
+    }
+
+    /// @notice Gets the parameters of the pool.
+    /// @param tokenA The address of the first token.
+    /// @param tokenB The address of the second token.
+    /// @return params The parameters of the pool.
+    function getParams(address tokenA, address tokenB) public view returns (IEulerSwap.Params memory params) {
+        address eulerSwap = getPool(tokenA, tokenB);
+        require(eulerSwap != address(0), PoolNotFound());
+        params = IEulerSwap(eulerSwap).getParams();
     }
 
     function previewEscrowVault() external view returns (address vault) {
